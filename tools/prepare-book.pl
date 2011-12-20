383bc9f2eedf598a384adbc203c2b0ef8b7aace4
Petya Kohts <petya@kohts.com>
2011-12-20 00:59:07 +0000

    * moved write_file_scalar into Yandex::Tools * support for local Yandex::Tools library

--- tools/prepare-book.pl
+++ tools/prepare-book.pl
@@ -3,30 +3,24 @@
 BEGIN {
   die "ARCHIVE_HOME environment variable must be set. Unable to continue\n\n"
     unless $ENV{'ARCHIVE_HOME'};
+
+  if (-d "/home/petya/CPAN/Yandex--Tools") {
+    $ENV{'TEST_LIB'} = "/home/petya/CPAN/Yandex--Tools/lib";
+  }
+  else {
+    $ENV{'TEST_LIB'} = "/";
+  }
 };
 
 use strict;
 use warnings;
 
 use lib "$ENV{'ARCHIVE_HOME'}/tools/lib";
+use lib "$ENV{'TEST_LIB'}";
 use Docbook::Archive;
 
 use Yandex::Tools;
 use Data::Dumper;
-
-sub write_file_scalar {
-  my ($filename, $value, $opts) = @_;
-
-  $opts = {} unless $opts;
-  my $fh = Yandex::Tools::safe_open($filename, "overwrite", {'timeout' => $opts->{'timeout'} || 2});
-  return 0 unless $fh;
-
-  $value = "" unless defined($value);
-
-  print $fh $value;
-  Yandex::Tools::safe_close($fh);
-}
-
 
 my $steps = {
   '1' => { 'filename_stdout_tool' => 'iod/fix_blockqoute_in_para.pl', },
@@ -63,7 +57,7 @@ foreach my $stepnumber (sort keys %{$steps}) {
       }
 
       if ($before_processing ne $processed->{'stdout'}) {
-        if (!write_file_scalar($v->{'absolute_name'}, $processed->{'stdout'})) {
+        if (!Yandex::Tools::write_file_scalar($v->{'absolute_name'}, $processed->{'stdout'})) {
           die "Unable to write step output to [$v->{'absolute_name'}]";
         }
         Yandex::Tools::debug("updated: $docbook_file");
