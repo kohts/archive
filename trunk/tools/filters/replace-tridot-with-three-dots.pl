1ead6257b222e70848b0b3e5a5ffa02d96634fd2
Petya Kohts <petya@kohts.com>
2013-05-12 12:21:53 +0000

    * of-10141-0039, of-10141-0045, of-10141-0093, of-10141-0095, of-10141-0460, of-10141-0582 compile * tools/filters/replace-tridot-with-three-dots.pl and process afk-works

---
+++ trunk/tools/filters/replace-tridot-with-three-dots.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use utf8;
+binmode STDOUT, ':encoding(UTF-8)';
+
+if (!$ARGV[0]) {
+  die "usage: $0 filename\n";
+}
+
+my $f;
+open($f, $ARGV[0]) || die "unable to open [$ARGV[0]]";
+binmode $f, ':encoding(UTF-8)';
+while (my $line = <$f>) {
+    $line =~ s/…/.../g;
+    print $line;
+}
+close($f);
