aa660512678a92f5793839277f5ed3f4a1461ce0
Petya Kohts <petya@kohts.com>
2015-12-20 16:05:21 +0300

    preparing for archive import in sdm
    * allow user-defined config for tsv_to_dsplace.pl
    
    * fixed typo while doing --initial-import (which determines
    whether there're scanned images for the collection); only required
    for validation, not important for initial import.
    
    * added must_exist mode for read_scanned_docs(),
    read_docbook_sources(), read_ocr_html_docs() - to give
    a hint to the user to do configuration during --import-bitstreams

---
+++ trunk/tools/.tsv_to_dspace.pl
@@ -0,0 +1,8 @@
+our $config = {
+    'external_archive_storage_base' => '',
+    'docbook_source_base' => '',
+    'docbook_dspace_html_out_base' => '',
+    'docbook_dspace_pdf_out_base' => '',
+};
+
+return $config;
