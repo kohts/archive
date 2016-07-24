3f8eb6087f482ca491ee8843718ceea36c3a3eac
Petya Kohts <petya@kohts.com>
2016-07-24 19:39:56 +0300

    removed files older than half a year

--- trunk/bin/postgres_backup.sh
+++ trunk/bin/postgres_backup.sh
@@ -3,3 +3,4 @@
 for d in `psql -c "COPY (select datname from pg_database where datistemplate = false) TO STDOUT WITH CSV"` ; do
     pg_dump $d | gzip -c > /backup/postgres/pg_dump.$d.`date +%s`.sql.gz
 done
+find /backup/postgres/ -mtime +180 | xargs -ri rm {}
